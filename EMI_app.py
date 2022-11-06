5ee427b129f5a02aad3a36e800302bfe2c757b11
VishPrem <91227044+VishPrem@users.noreply.github.com>
2022-11-06 11:18:23 -0500

    Add files via upload

--- EMI_app.py
+++ EMI_app.py
@@ -5,9 +5,9 @@ def calculate_emi(p, n, r):
   return p * (r / 100) * ((1 + (r / 100)) ** n / ((1 + (r / 100)) ** n - 1))
 
 st.title('EMI Calculator App')
-principal = st.slider("Principal Loan Amount", 1000, 1000000)
-tenure = st.slider("Loan Period (in years)", 1, 30)
-roi = st.slider("Rate of Interest (in % per annum)", 1, 15)
+principal = 50000
+tenure = 2
+roi = 5.05
 
 n = tenure * 12
 r = roi / 12
